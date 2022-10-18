eaaea4e3e7cf58586eecab7f48c2cda6cb3ead50
Emiko Esther S <graceupongraceemi@gmail.com>
2022-10-18 15:59:52 -0700

    Nav component updated, auth folder and login.js created in pages folder

--- components/Nav.js
+++ components/Nav.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import Link from 'next/link';
 
 const Nav = () => {
   return (
-    <div>
-      <h1>Nav</h1>
-    </div>
+    <nav className='flex justify-between items-center py-10'>
+      <Link href='/'>
+        <button className='text-lg font-medium'>Message Journals</button>
+      </Link>
+      <ul className='flex items-center gap-10'>
+        <Link href={'/auth/login'}>
+          <a className='py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8'>
+            Join Now
+          </a>
+        </Link>
+      </ul>
+    </nav>
   );
 };
 
