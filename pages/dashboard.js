27cb83da3da7788041b899b45f34e5e46ed81ac9
Emiko Esther S <graceupongraceemi@gmail.com>
2022-10-18 19:24:19 -0700

    In the Nav.js, user authentification to post or join added and dashboard.js is created in pages

---
+++ pages/dashboard.js
@@ -0,0 +1,11 @@
+const dashboard = () => {
+  return (
+    <div>
+      <h1>Your posts</h1>
+      <div>posts</div>
+      <button>Sign Out</button>
+    </div>
+  );
+};
+
+export default dashboard;
