bc9bb1ae63a4a1890f7734ef6911a1dd0ae63d3f
Emiko Esther S <graceupongraceemi@gmail.com>
2022-10-18 18:46:55 -0700

    In the login.js, installed react-firebase-hooks and next.js router

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,15 +1,31 @@
 import { FcGoogle } from 'react-icons/fc';
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { auth } from '../../utils/firebase';
+import { useRouter } from 'next/router';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useEffect } from 'react';
 
 const login = () => {
+  const route = useRouter();
+  const [user, loading] = useAuthState(auth);
   // Sign in with Google
   const googleProvider = new GoogleAuthProvider();
   const GoogleLogin = async () => {
     try {
       const result = await signInWithPopup(auth, googleProvider);
-    } catch (error) {}
+      route.push('/');
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  useEffect(() => {
+    if (user) {
+      route.push('/');
+    } else {
+      console.log('logon');
+    }
+  }, [user]);
 
   return (
     <div className='shadow-xl mt-32 p-10 text-gray-700 rounded-lg'>
