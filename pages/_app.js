57333b73e818211c7570eae39b7d008ccc328284
Emiko Esther S <graceupongraceemi@gmail.com>
2022-10-20 14:08:04 -0700

    react-toastify installed and added to pages _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
+import { ToastContainer } from 'react-toastify';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <ToastContainer />
       <Component {...pageProps} />
     </Layout>
   );
